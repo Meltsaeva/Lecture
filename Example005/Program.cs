d6b352043a538a9ed1c2530e55f6ec4cc7b1dd2f
Anastasiia <Batrakovaad@yndex.ru>
2023-02-13 11:34:21 +0700

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("What's your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "anastasiia")
+{
+    Console.WriteLine("Waa, who is it?");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
