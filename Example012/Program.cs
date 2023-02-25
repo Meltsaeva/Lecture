1db83f931e68dde7528a43cc2c7e258ba81bb150
Anastasiia <Batrakovaad@yndex.ru>
2023-02-25 19:20:00 +0700

    finish2

--- Example012/Program.cs
+++ Example012/Program.cs
@@ -78,33 +78,31 @@ for (int i = 2; i <= 10; i++)
 // маленькие буквы “к” заменить большими “К”,
 // а большие “С” маленькими “с”.
 
-// string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
-//                 + "ежели бы вас послали вместо нашего милого Винценгероде,"
-//                 + "вы бы взяли приступом согласие прусского короля."
-//                 + "Вы так красноречивы. Вы дадите мне чаю?";
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+                + "ежели бы вас послали вместо нашего милого Винценгероде,"
+                + "вы бы взяли приступом согласие прусского короля."
+                + "Вы так красноречивы. Вы дадите мне чаю?";
 
-//string s = "qwerty"
+// string s = "qwerty"
 //            012345
 // s[3] // r
 
-// string Replace(string text, char oldValue, char newValue)
-// {
-//     string result = String.Empty;
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
 
-//     int length = text.Length;
-//     for (int i = 0; i < length; i++)
-//     {
-//         if(text[i] == oldValue) result = result + $"{newValue}";
-//         else result = result + $"{text[i]}";
-//     }
-//     return result;
-// }
-// string newText = Replace(text, ' ', '|');
-// Console.WriteLine(newText);
-// newText = Replace(text, 'к', 'К');
-// Console.WriteLine(newText);
-// newText = Replace(text, 'С', 'с');
-// Console.WriteLine(newText);
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+newText = Replace(newText, 'к', 'К');
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
 
 int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
 void PrintArray(int[] array)
